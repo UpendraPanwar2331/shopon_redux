8331280a07446bf5b82d5ebb45e5aa324420b665
upendra <upendra2331@gmail.com>
2023-04-17 11:51:37 +0530

    hello

--- src/template/Footer.js
+++ src/template/Footer.js
@@ -1,31 +1,67 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
     return (
         <div>
-            <div className='row footer-area'>
-                <div className='col-2'>
-                    Become a Seller
+            <div class="row" style={{ backgroundColor: '#172337'}}>
+                <div class="col-sm-3 mb-3 mb-sm-0">
+                    <div>
+                        <div class="card-body" style={{ backgroundColor: '#172337', color: 'white' }}>
+                            <h5 class="card-title">ABOUT</h5>
+                            <p class="card-text">About us</p>
+                            <p class="card-text">Contact us</p>
+                            <p class="card-text">Carriers</p>
+
+                            <Link href="#" style={{ textDecoration: 'none', color: 'yellow', fontFamily: 'Arial' }}>Become a Seller</Link>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div>
+                        <div class="card-body" style={{ backgroundColor: '#172337', color: 'white' }}>
+                            <h5 class="card-title">HELP</h5>
+                            <p class="card-text">Payment</p>
+                            <p class="card-text">Shipping</p>
+                            <p class="card-text">Cancellation & Return</p>
+                            <Link href="#" style={{ textDecoration: 'none', color: 'yellow', fontFamily: 'Arial' }}>Advertise</Link>
+                        </div>
+                    </div>
                 </div>
 
-                <div className='col-2'>
-                    Advertise
+                <div class="col-sm-3">
+                    <div>
+                        <div style={{ backgroundColor: '#172337', color: 'white' }}>
+                            <h5 class="card-title">CUSTOMER POLICY</h5>
+                            <p class="card-text">Return Policy</p>
+                            <p class="card-text">Terms Of Use</p>
+                            <p class="card-text">Security</p>
+
+                            <Link href="#" style={{ textDecoration: 'none', color: 'yellow', fontFamily: 'Arial' }} >Gift cards</Link>
+                        </div>
+                    </div>
                 </div>
 
-                <div className='col-2'>
-                    Gift Cards
-                </div>
+                <div class="col-sm-3">
+                    <div>
+                        <div  style={{ backgroundColor: '#172337', color: 'white' }}>
+                            <h5 class="card-title">Mail us</h5>
+                            <p class="card-text">ShopOn Internet Private Limited,
 
-                <div className='col-2'>
-                    Help Center
-                </div>
+Buildings Alyssa, Begonia &
 
-                <div className='col'>
-                    © 2007-2023 Flipkart.com
-                </div>
+Clove Embassy Tech Village,
 
+Outer Ring Road, Devarabeesanahalli Village,
+
+Bengaluru, 560103,
+
+Karnataka, India</p>
+                            <Link href="#" style={{ textDecoration: 'none', color: 'yellow', fontFamily: 'Arial' }}>Go somewhere</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
-
         </div>
     )
 }
--- src/template/Menubar.js
+++ src/template/Menubar.js
@@ -8,37 +8,37 @@ const Menubar = () => {
       <div className='row'>
         <div className='col-12'>
         <img src={image} alt="img" style={{ display: "block", width: "100%" }} />
-      
-        <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#ffe6e6" }}>
+        </div>
+        <nav className="navbar navbar-expand-lg row"  style={{ backgroundColor: "#ffe6e6" }}>
 
   <div className="container-fluid">
-    <Link className="navbar-brand" to='/home'>Home</Link>
+    <Link className="navbar-brand" to='/home'>&nbsp;&nbsp;&nbsp;&nbsp;Home</Link>
     {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button> */}
     <div className="collapse navbar-collapse" id="navbarNav">
     <ul className="navbar-nav">
   <li className="nav-item">
-    <Link className="nav-link" to='/category/electronics' style={{fontSize: '20px', fontWeight: 'bold'}}>Electronics</Link>
+    <Link className="nav-link" to='/category/electronics' style={{fontSize: '20px', fontWeight: 'bold', color:'black'}}>Electronics</Link>
   </li>
 
   <li className="nav-item">
-    <Link className="nav-link" to='/category/jewelery' style={{fontSize: '20px', fontWeight: 'bold'}}>Jewelry</Link>
+    <Link className="nav-link" to='/category/jewelery' style={{fontSize: '20px', fontWeight: 'bold' , color:'black'}}>Jewelry</Link>
   </li>
 
   <li className="nav-item">
-    <Link className="nav-link" to="/category/men's clothing" style={{fontSize: '20px', fontWeight: 'bold'}}>Mens' Clothing</Link>
+    <Link className="nav-link" to="/category/men's clothing" style={{fontSize: '20px', fontWeight: 'bold' , color:'black'}}>Mens' Clothing</Link>
   </li>
 
   <li className="nav-item">
-    <Link className="nav-link" to="/category/women's clothing" style={{fontSize: '20px', fontWeight: 'bold'}}>Womens's Clothing</Link>
+    <Link className="nav-link" to="/category/women's clothing" style={{fontSize: '20px', fontWeight: 'bold' , color:'black'}}>Womens's Clothing</Link>
   </li>
 </ul>
     </div>
   </div>
 </nav>
     
-        </div>
+      
       </div>
     </div>
   )
